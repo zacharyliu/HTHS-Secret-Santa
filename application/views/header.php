9db3f62d54da6baa69bfe9cf455dea8bdbbd40e8
matt <gears88@gmail.com>
2012-12-07 23:43:39 -0500

    poopy poopy poop

---
+++ application/views/header.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36741494-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<link rel="stylesheet" type="text/css" href="/application/views/main.css">
+</head>
+
+<body>
+<div style="width:100%;height:50px;margin:0;padding:0;">
+
+	<a class="title" href="<?php echo base_url("calendar")?>">Don't Break the Chain</a> 
+
+	<span style="width:auto;margin:0px;padding:0px;float:right;">
+		<a href="<?php echo base_url("calendar/login-register")?>">login/register</a>&nbsp;
+	</span>
+		
+	</br>
+	<a class="subheader" href="<?php echo base_url("calendar/about")?>">About</a> 
+	
+</div>
+</body>
