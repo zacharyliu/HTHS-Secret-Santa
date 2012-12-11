70cdba9d0d72be7c54e472fcc59a0327fdc5764c
vincent <vincentchen96@gmail.com>
2012-12-11 18:05:03 -0500

    about test

---
+++ application/views/about.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/application/views/main.css">
+
+</head>
+
+<body>
+
+<div style="margin-top:25px;margin-left:120px;margin-right:120px;">
+	
+	
+	<p> 
+	<b>Test</b> 
+	</br>
+	
+</div>
+
+</body>
+</html>
