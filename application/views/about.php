6bb21e37116700f62c213fca28eb79a7e39807ab
vincent <vincentchen96@gmail.com>
2012-12-11 19:03:30 -0500

    more questions

--- application/views/about.php
+++ application/views/about.php
@@ -13,7 +13,11 @@
 	HTHS Secret Santa is a web app built by and for HTHS students. By using technology to simplify the 
 	<a href="http://www.wikihow.com/Do-a-Secret-Santa"> Secret Santa</a> 
 	process, we are able to ensure that the activity runs smoothly. :)</p>
-
+	
+	<p> <b> How do I register? </b> </br>
+	Follow the instructions in the header to <i> "login/register" </i>. Be sure to use your "ctemc.org" 
+	email when registering!  </p>
+	
 	<p> <b> What if I want to have a Secret Santa among my own friends? </b> </br>
 	We implemented a unique "groups" feature that allows you to participate in multiple Secret Santas.
 	After you login/register, you are given to option to create your own group that is matched with a
@@ -21,8 +25,25 @@
 	same Secret Santa group. </p>
 	
 	<p> <b> What is the price range for gifts? </b> </br>
-	$10-$20. Be thoughtful! Don't get anything too big, or awkwardness ensues. </p>
+	$10-$20. Be thoughtful! Don't get anything too expensive or it will be awkward. </p>
 	
+	<p> <b> What kinds of gifts should I  buy? </b> </br> 
+	You can buy anything that you think that your exchange partner would enjoy! Just make sure that it's
+	appropriate to bring into school. Don't be dumb. </p>
+	
+	<p> <b> What if I get myself? </b> </br>
+	You won't. Believe in Matt Hsu. </p>
+	
+	<p> <b> What do I do if I don't want to participate anymore? </b> </br>
+	You can remove yourself from any group as long as you do so before <i> Friday, 12/14 (when registration
+	ends). </i> Otherwise, don't back out of your commitments, or there will be repercussions. Don't be that guy.
+	</p> 
+
+	
+	<span style="text-align:center">
+	<h3> <i> Enjoy! </i> </h3>
+	</span>
+
 	
 </div>
 
