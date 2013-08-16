54b72ce055e10e6df94bd87fc632892ae8f95aa2
matt <gears88@gmail.com>
2013-08-16 18:15:49 -0400

    [fix] deny directory access

--- application/views/about.php
+++ application/views/about.php
@@ -9,16 +9,16 @@
 <div style="margin-top:auto;margin-left:30px;margin-right:30px;">
     <h2> Frequently Asked Questions </h2>
 
-    <p><b> What is HTHS Secret Santa? </b> </br>
+    <p><b> What is HTHS Secret Santa? </b> </br/>
         HTHS Secret Santa is a web app built by and for HTHS students. By using technology to simplify the
         <a href="http://www.wikihow.com/Do-a-Secret-Santa"> Secret Santa</a>
         process, we are able to ensure that the activity runs smoothly. :)</p>
 
-    <p><b> How do I register? </b> </br>
+    <p><b> How do I register? </b> <br/>
         Follow the instructions in the header to <i> "login/register" </i>. Be sure to use your "ctemc.org"
         email when registering! </p>
 
-    <p><b> What if I want to have a Secret Santa among my own friends? </b> </br>
+    <p><b> What if I want to have a Secret Santa among my own friends? </b> <br/>
         We implemented a unique "groups" feature that allows you to participate in multiple Secret Santas.
         After you login/register, you are given to option to create your own group that is matched with a
         generated code. Share this code with your friends. If all of you input the same code, you'll be in the
@@ -32,10 +32,10 @@
         You can buy anything that you think that your exchange partner would enjoy! Just make sure that it's
         appropriate to bring into school. Don't be dumb. </p>
 
-    <p><b> What if I get myself? </b> </br>
+    <p><b> What if I get myself? </b> <br/>
         You won't. Believe in Matt Hsu. </p>
 
-    <p><b> What do I do if I don't want to participate anymore? </b> </br>
+    <p><b> What do I do if I don't want to participate anymore? </b> <br/>
         You can remove yourself from any group as long as you do so before <i> Friday, 12/14 (when registration
             ends). </i> Otherwise, don't back out of your commitments, or there will be repercussions. Don't be that
         guy.
@@ -49,8 +49,7 @@
 
     <p>
     <li><b> Web Dev: </b> Vincent Chen; Matt Hsu; Zachary Liu</li>
-    <li><b> Logo Design: </b> Kevin Herviou
-        </p>
+    <li><b> Logo Design: </b> Kevin Herviou</li>
 
 </div>
 
