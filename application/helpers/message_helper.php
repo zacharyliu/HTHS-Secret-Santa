d0f208e2b6f1b225743d8a69174b535dbcc80ea5
matt <gears88@gmail.com>
2013-08-16 22:43:28 -0400

    [new] added message helper [new] began navbar formatting in main.less

---
+++ application/helpers/message_helper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * assists in the creation of custom information, warning, or error messages based on the bootstrap formatting
+ *
+ * @param string $message               message to be displayed (raw html)
+ * @param int $warninglevel             0-2; 0 is least critical, 2 is most critical
+ *        0 = blue info
+ *        1 = green success
+ *        2 = yellow warning
+ *        3 = red error
+ * @return string                       fully generated html of warning label
+ */
+function message($message = '', $warninglevel = 0)
+{
+    $dismiss = '<button type="button" class="close" data-dismiss="alert">&times;</button>';
+    switch ($warninglevel) {
+        case 0:
+            return '<div class="alert alert-info">' . $dismiss . $message . '</div>';
+        case 1:
+            return '<div class="alert alert-success">' . $dismiss . $message . '</div>';
+        case 2:
+            return '<div class="alert">' . $dismiss . $message . '</div>';
+        case 3:
+            return '<div class="alert alert-error">' . $dismiss . $message . '</div>';
+    }
+}
--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,4 +1,54 @@
-<div style="width:100%;height:50px;margin:0;padding:0;">
+<!--NAVBAR-->
+<div class="navbar navbar-inverse navbar-fixed-top ">
+    <button type="button" id="nav-btn" class="navbar-toggle" data-type="open" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="/"><img id="title-icon" src="../img/app-icon.png" height="45px" width="45px" /><span id="title">HTHS Secret Santa</span></a>
+
+    <div class="navbar-collapse collapse" style="overflow: hidden;">
+        <ul class="nav navbar-nav navbar-left">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <?php
+            if ($this->session->userdata('auth') == 'true') {
+                echo '<li><p>Welcome back, ' . $this->session->userdata('name') . '.</p></li>';
+                if ($this->session->userdata('admin') == 'true')
+                    echo '<li><a href="' . base_url("admin") . '">Admin Panel</a>';
+                echo '<li><a href="/profile">Profile</a></li>' . '<li><a href="/login/logout">Logout</a>';
+            } else {
+                echo '<li><a href="/login">Login / Register</a></li>';
+            }?>
+        </ul>
+    </div>
+</div>
+
+           <!-- <div class="nav-collapse collapse">
+                <ul class="nav">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+                <ul class="nav pull-right">
+                    <?php
+                    if ($this->session->userdata('auth') == 'true') {
+                        echo '<li><a style="background: rgba(0,0,0,0)">Welcome back, ' . $this->session->userdata('name') . '.</a></li>';
+                        if ($this->session->userdata('admin') == 'true')
+                            echo '<li><a href="' . base_url("admin") . '">Admin Panel</a>';
+                        echo '<li><a href="/profile">Profile</a></li>' . '<li><a href="/login/logout">Logout</a>';
+                    } else {
+                        echo '<li><a href="/login">Login / Register</a></li>';
+                    }?>
+
+                </ul>
+            </div>
+            <!--/.nav-collapse
+        </div>
+    </div>
+</div>
+<!--<div style="width:100%;height:50px;margin:0;padding:0;">
 
     <?php
     $link = 'index'; // url link
@@ -24,4 +74,4 @@
 	</span>
 </div>
 </br> </br>  </br>
-<hr>
+<hr>-->
