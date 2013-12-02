ffefb52c66c088d5b38fbc38ac939658c31bd774
Zachary Liu <liu.zachary@gmail.com>
2013-12-01 21:59:27 -0500

    Transpose the globalvars table to allow more possible variables.

--- database.sql
+++ database.sql
@@ -21,10 +21,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `globalvars` (
-  `firstyear` smallint(4) NOT NULL COMMENT 'The first year that data exists for',
-  `registration` tinyint(1) NOT NULL,
-  `maxgroups` int(2) NOT NULL,
-  UNIQUE KEY `firstyear` (`firstyear`)
+  `key` varchar(20) NOT NULL,
+  `value` text NOT NULL,
+  UNIQUE KEY `key` (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='secretsanta global variables';
 
 -- --------------------------------------------------------
