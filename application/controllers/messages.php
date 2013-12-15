d08e3839f5dc22d1ae355c727dc351fc5eab4f04
Zachary Liu <liu.zachary@gmail.com>
2013-12-15 17:32:20 -0500

    Message controller sending logic and routing.

--- application/controllers/messages.php
+++ application/controllers/messages.php
@@ -8,10 +8,28 @@ class Messages extends CI_Controller {
             redirect(base_url("login/timeout"));
         }
         $this->load->model('messagesmod');
+        $this->load->model('datamod');
+        $this->user_id = $this->session->userdata('id');
+        $this->current_year = intval(date('Y'));
     }
 
     public function index() {
+        $threads = $this->messagesmod->listThreads($this->session->userdata('id'));
+        render('messages', array('threads' => $threads));
+    }
 
+    public function send($code) {
+        $pair = $this->datamod->getPair($code, $this->user_id);
+
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('message', 'Message', 'trim|required|xss_clean');
+
+        if (!$this->form_validation->run()) {
+            render('messages_send', array('code' => $code));
+        } else {
+            $this->messagesmod->send($pair, null, $code, $this->input->post('message'));
+            redirect('messages');
+        }
     }
 
 }
