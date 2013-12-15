d44f9f33b5291b3cd8fa1caac466ff122a1f53eb
Zachary Liu <liu.zachary@gmail.com>
2013-12-15 17:46:05 -0500

    Messages inbox view.

--- application/views/messages.php
+++ application/views/messages.php
@@ -1,1 +1,24 @@
-<?php
+<div class="container">
+    <h2>Inbox</h2>
+    <table class="table table-hover">
+        <tr>
+            <th>Date</th>
+            <th>Group</th>
+            <th>Latest Message</th>
+        </tr>
+        <?php if (count($threads) == 0) : ?>
+        <tr>
+            <td colspan="3" style="text-align: center;">
+                No messages received!
+            </td>
+        </tr>
+        <?php endif; ?>
+        <?php foreach ($threads as $thread) : ?>
+        <tr>
+            <td><?=$thread->timestamp?></td>
+            <td><?=$thread->code?></td>
+            <td><?=$thread->message?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
