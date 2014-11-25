5b44a6a3c125df466526ccb688dfc8e72e3cf901
Zachary Liu <liu.zachary@gmail.com>
2014-11-25 02:28:23 -0500

    Initial CAS support

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -12,27 +12,30 @@ class Login extends CI_Controller
      */
     public function index()
     {
-        require(APPPATH . 'classes/openid.php');
+        require(APPPATH . 'classes/CAS.php');
 
-        $openid = new LightOpenID($_SERVER['HTTP_HOST']);
-        if (!$openid->mode) {
-            // Didn't get login info from the OpenID provider yet / came from the login link
-            $openid->identity = 'https://www.google.com/accounts/o8/id';
-            $openid->required = array('namePerson/first', 'namePerson/last', 'contact/email');
-            header('Location: ' . $openid->authUrl());
-        } else if ($openid->mode == 'cancel') {
-            // The user decided to cancel logging in, so we'll redirect to the home page instead
-            redirect('/');
-        } else {
+        phpCAS::setDebug();
+
+        // initialize phpCAS
+        phpCAS::client(CAS_VERSION_2_0,'fed.princeton.edu',443,'cas');
+
+        // no SSL validation for the CAS server
+        phpCAS::setNoCasServerValidation();
+
+        // force CAS authentication
+        phpCAS::forceAuthentication();
+
+        if (true) {
             // The user has logged in and the user's info is ready
-            if (!$openid->validate()) {
-                // Authentication failed, try logging in again
-                $this->login_failure('Authentication failed, try logging in again.');
-            } else {
+            if (true) {
                 // Authentication was successful
 
                 // Get user attributes:
-                $user_data = $openid->getAttributes();
+                $user_data = array(
+                    "namePerson/first" => phpCAS::getUser(),
+                    "namePerson/last" => phpCAS::getUser(),
+                    "contact/email" => phpCAS::getUser() . "@princeton.edu"
+                );
 
                 // Check to make sure that the user is logging in using a @ctemc.org account or email exception:
                 $this->load->model('datamod');
